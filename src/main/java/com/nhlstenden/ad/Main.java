b6432d0669fb300c360220565d5f282fcf830b6c
VictorP03 <crapowner321@gmail.com>
2023-03-29 22:04:42 +0200

    Finished making remove function.

--- src/main/java/com/nhlstenden/ad/Main.java
+++ src/main/java/com/nhlstenden/ad/Main.java
@@ -6,10 +6,11 @@ public class Main {
     public static void main(String[] args) {
         TreeMap treeMap = new TreeMap();
         treeMap.add(treeMap.root, 5, 12);
-        treeMap.add(treeMap.root, 4, 11);
-        treeMap.add(treeMap.root, 6, 15);
-        treeMap.add(treeMap.root, 3, 10);
-        treeMap.add(treeMap.root, 2, 9);
+        treeMap.add(treeMap.root, 2, 11);
+        treeMap.add(treeMap.root, 8, 15);
+        treeMap.add(treeMap.root, 6, 10);
+        treeMap.add(treeMap.root, 9, 9);
+        treeMap.remove(treeMap.root, 8);
         treeMap.printNodes(treeMap.root);
     }
 }
--- src/main/java/com/nhlstenden/ad/treemap/TreeMap.java
+++ src/main/java/com/nhlstenden/ad/treemap/TreeMap.java
@@ -1,11 +1,16 @@
 package com.nhlstenden.ad.treemap;
 
+import java.util.ArrayList;
+
 public class TreeMap
 {
     public Node root;
 
+    private ArrayList<Node> nodes;
+
     public TreeMap() {
         root = new Node(10, 15);
+        nodes = new ArrayList<>();
     }
 
     public Node add(Node parent, int key, int value) {
@@ -22,7 +27,10 @@ public class TreeMap
         }
     }
 
-    /*public Node remove(Node parent, int key) {
+    public Node remove(Node parent, int key) {
+        if (parent == null) {
+            return null;
+        }
         if (parent.key != key) {
             if (key < parent.key) {
                 parent.left = remove(parent.left, key);
@@ -33,14 +41,51 @@ public class TreeMap
         } else {
             if (parent.left == null) {
                 return parent.right;
-            } else if (parent.right == null) {
-                return parent.left;
-            } else {
-
             }
-
+            if (parent.right == null) {
+                return parent.left;
+            }
+            Node newParent = getSmallestNode(parent.right);
+            parent.key = newParent.key;
+            parent.value = newParent.value;
+            parent.right = remove(parent.right, newParent.key);
         }
+        return parent;
+    }
+
+    /*public void restructureTree(ArrayList<Node> nodes) {
+        nodes.remove(0);
+        for (Node node : nodes) {
+            System.out.println(node.key);
+        }
+        nodes.sort((node1, node2) -> {
+            if (node1.key == node2.key) {
+                return 0;
+            }
+            return node1.key < node2.key ? -1 : 1;
+        });
+        for (int i = 1; i < nodes.size(); i++) {
+            add(root, nodes.get(i).key, nodes.get(i).value);
+        }
+        this.nodes.clear();
+        //return newParent;
+    }
+
+    public ArrayList<Node> getNodes(Node parent) {
+        if (parent != null) {
+            this.nodes.add(parent);
+            getNodes(parent.left);
+            getNodes(parent.right);
+        }
+        return nodes;
     }*/
+
+    public Node getSmallestNode(Node parent) {
+        while (parent.left != null) {
+            parent = parent.left;
+        }
+        return parent;
+    }
 
     public void printNodes(Node parent) {
         //Node tree = root;
