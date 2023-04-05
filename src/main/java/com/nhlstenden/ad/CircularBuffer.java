ebf2ddd82ec76bded5c245590dfe30f8862ce81f
rickv21 <rick.vinke@student.nhlstenden.com>
2023-04-05 13:14:28 +0200

    Added bubble sorting to the buffer

--- src/main/java/com/nhlstenden/ad/CircularBuffer.java
+++ src/main/java/com/nhlstenden/ad/CircularBuffer.java
@@ -1,32 +1,92 @@
 package com.nhlstenden.ad;
 
+import java.util.Comparator;
 import java.util.NoSuchElementException;
+import java.util.function.Function;
 
-public class CircularBuffer<T> {
-    private final int capacity;
-    private final Object[] buffer;
-    private int head = 0;
-    private int tail = 0;
-    private int size = 0;
+public class CircularBuffer<T extends Comparable<T>> {
+    private final T[] buffer;
+    private int head;
+    private int tail;
+    private int capacity;
+    private int size;
+    private Comparator<T> comparator;
 
-    public CircularBuffer(int capacity) {
+    @SuppressWarnings("unchecked")
+    public CircularBuffer(int capacity, Comparator<T> comparator) {
+        this.buffer = (T[]) new Comparable[capacity];
+        this.head = 0;
+        this.tail = -1;
         this.capacity = capacity;
-        buffer = new Object[capacity];
+        this.size = 0;
+        this.comparator = comparator;
     }
 
-    /**
-     * Adds the given item to the buffer.
-     *
-     * @param item The item to bt added to the buffer.
-     */
-    public void add(T item) {
+    public void add(T element) {
+        // Calculating the index to add the element
+        int index = (tail + 1) % capacity;
+
+        // Size of the array increases as elements are added
+        size++;
+
+        // Checking if the array is full
         if (size == capacity) {
-            tail = (tail + 1) % capacity;
-        } else {
-            size++;
+           // throw new Exception("Buffer Overflow");
+            return;
         }
-        buffer[head] = item;
-        head = (head + 1) % capacity;
+
+        // Storing the element in the array
+        buffer[index] = element;
+
+        // Incrementing the tail pointer to point
+        // to the element added currently
+        tail++;
+    }
+
+    public void bubbleSort(Function<T, Comparable> keyExtractor) {
+        boolean swapped = true;
+
+        while (swapped) {
+            swapped = false;
+
+            for (int i = head; i < tail - 1 || (tail == head && i == tail - 1); i++) {
+                T current = buffer[i];
+                T next = buffer[(i + 1) % capacity];
+
+                keyExtractor.apply(current);
+                keyExtractor.apply(next);
+
+                if (comparator.compare(current, next) > 0) {
+                    buffer[i] = next;
+                    buffer[(i + 1) % capacity] = current;
+                    swapped = true;
+                }
+            }
+
+            tail = (tail - 1 + capacity) % capacity;
+
+            if (!swapped) {
+                break;
+            }
+
+            swapped = false;
+
+            for (int i = tail; i > head || (tail == head && i == head); i--) {
+                T current = buffer[i];
+                T prev = buffer[(i - 1 + capacity) % capacity];
+
+                keyExtractor.apply(current);
+                keyExtractor.apply(prev);
+
+                if (comparator.compare(current, prev) < 0) {
+                    buffer[i] = prev;
+                    buffer[(i - 1 + capacity) % capacity] = current;
+                    swapped = true;
+                }
+            }
+
+            head = (head + 1) % capacity;
+        }
     }
 
     /**
@@ -35,20 +95,33 @@ public class CircularBuffer<T> {
      * @return The item that was removed.
      */
     public T removeLast() {
-        if (size == 0) {
+        if (capacity == 0) {
             throw new NoSuchElementException("The buffer is empty!");
         }
         T item = (T) buffer[tail];
         tail = (tail + 1) % capacity;
-        size--;
+        capacity--;
         return item;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < capacity; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(buffer[i]);
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+
+    public int getCapacity() {
+        return capacity;
     }
 
     public int getSize() {
         return size;
-    }
-
-    public boolean isFull() {
-        return size == capacity;
     }
 }
--- src/main/java/com/nhlstenden/ad/Main.java
+++ src/main/java/com/nhlstenden/ad/Main.java
@@ -2,6 +2,20 @@ package com.nhlstenden.ad;
 
 public class Main {
     public static void main(String[] args) {
+        CircularBuffer<Integer> buffer = new CircularBuffer<>(5, Integer::compare);
+        buffer.add(2);
+        buffer.add(9);
+        buffer.add(1);
+        buffer.add(6);
 
+        buffer.add(3);
+
+        buffer.add(20);
+        System.out.println(buffer.toString());
+
+        buffer.bubbleSort(Integer::intValue);
+
+        System.out.println("after");
+        System.out.println(buffer.toString());
     }
 }
